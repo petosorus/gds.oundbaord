15370a605e5afe71c8eec18b307ddd76fc6e00e8
petosorus <petosorus@hotmail.fr>
2024-05-27 21:56:21 +0200

    recherche basique

--- main.js
+++ main.js
@@ -9,16 +9,49 @@ var contenu = [
         ],
         "script": "prout",
         "chemin": "samples/fart/fart.mp3"
+    }, {
+        "titre": "test 2",
+        "protagonistes": [
+            "thomas",
+            "roy de poy"
+        ],
+        "script": "prout",
+        "chemin": "samples/fart/fart.mp3"
+    }, {
+        "titre": "test 3",
+        "protagonistes": [
+            "thomas",
+            "roy de poy"
+        ],
+        "script": "test 3",
+        "chemin": "samples/fart/fart.mp3"
     }
 ];
 
-contenu.forEach((sound) => {
-    const elem = document.createElement("sample-element-slots");
-    elem.setAttribute("sound", sound.chemin);
-    const titre = document.createElement('h1');
-    titre.setAttribute("slot", "title");
-    titre.innerHTML = sound.titre;
-    
-    elem.appendChild(titre);
-    contenuElement.appendChild(elem)
+var contenuFiltrey = []
+
+function afficherListe(liste) {
+    contenuElement.innerHTML = "";
+
+    liste.forEach((sound) => {
+        const elem = document.createElement("sample-element-slots");
+        elem.setAttribute("sound", sound.chemin);
+        const titre = document.createElement('h1');
+        titre.setAttribute("slot", "title");
+        titre.innerHTML = sound.titre;
+        
+        elem.appendChild(titre);
+        contenuElement.appendChild(elem)
+    });
+}
+
+afficherListe(contenu);
+
+document.getElementById('recherche').addEventListener('input', (evenement) => {
+    const recherche = evenement.target.value;
+    contenuFiltrey = contenu.filter(element => {
+        return element.script.includes(recherche) || element.titre.includes(recherche);
+    });
+
+    afficherListe(contenuFiltrey);
 });
