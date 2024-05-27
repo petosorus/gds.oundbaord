7c4e2fb163b5ae3ae56618e69ac370da4e91e06d
petosorus <petosorus@hotmail.fr>
2024-05-27 22:17:25 +0200

    Recherche pondérée

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+const poidsRecherche = {
+    titre: 10,
+    script: 1,
+    protagonistes: 5
+}
+
 const contenuElement = document.getElementById('contenu');
 
 var contenu = [
@@ -20,15 +26,43 @@ var contenu = [
     }, {
         "titre": "test 3",
         "protagonistes": [
-            "thomas",
+            "personne",
             "roy de poy"
         ],
-        "script": "test 3",
+        "script": "test 3 poy",
         "chemin": "samples/fart/fart.mp3"
     }
 ];
 
 var contenuFiltrey = []
+
+function recherche(valeur) {
+    contenuFiltrey = [];
+
+    contenu.forEach(element => {
+        let score = 0;
+        if (element.titre.includes(valeur)) {
+            score += poidsRecherche.titre;
+        }
+        if (element.script.includes(valeur)) {
+            score += poidsRecherche.script;
+        }
+        
+        protagonistesFiltreys = element.protagonistes.filter(personnage => personnage.includes(valeur));
+        if (protagonistesFiltreys.length > 0) {
+            score += poidsRecherche.protagonistes;
+        }
+
+        if (score > 0) {
+            contenuFiltrey.push({
+                ...element,
+                score
+            });
+        }
+    });
+
+    contenuFiltrey.sort((a, b) => b.score - a.score);
+}
 
 function afficherListe(liste) {
     contenuElement.innerHTML = "";
@@ -45,13 +79,15 @@ function afficherListe(liste) {
     });
 }
 
-afficherListe(contenu);
+function installation() {
+    afficherListe(contenu);
 
-document.getElementById('recherche').addEventListener('input', (evenement) => {
-    const recherche = evenement.target.value;
-    contenuFiltrey = contenu.filter(element => {
-        return element.script.includes(recherche) || element.titre.includes(recherche);
+    document.getElementById('recherche').addEventListener('input', (evenement) => {
+        const valeur = evenement.target.value;
+        recherche(valeur);
+
+        afficherListe(contenuFiltrey);
     });
+}
 
-    afficherListe(contenuFiltrey);
-});
+installation();
