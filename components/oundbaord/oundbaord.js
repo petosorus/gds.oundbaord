df4ed30a833e2405fd537a37bb3c3bdf5bca857c
petosorus <petosorus@hotmail.fr>
2024-07-19 18:47:01 +0200

    stop play

--- components/oundbaord/oundbaord.js
+++ components/oundbaord/oundbaord.js
@@ -5,6 +5,9 @@
         "s-oundbaord",
         class extends HTMLElement {
             jouant = false;
+            playIcon
+            stopIcon
+            son
 
             constructor() {
                 super();
@@ -14,15 +17,26 @@
                 
                 const shadowRoot = this.attachShadow({ mode: "open" });
                 shadowRoot.appendChild(template.cloneNode(true));
+
+                this.playIcon = this.shadowRoot.querySelectorAll(".play")[0];
+                this.stopIcon = this.shadowRoot.querySelectorAll(".stop")[0];
             }
 
             connectedCallback() {
                 setTimeout(() => {
 
                     const button = this.shadowRoot.querySelectorAll(".boutton-lecture")[0];
+                    
                     button.addEventListener("click", () => {
                         if (!this.jouant) {
+                            this.stopIcon.style.display = 'block'
+                            this.playIcon.style.display = 'none'
+
                             this.jouerSon();
+                        } else {
+                            this.son.pause()
+                            this.son.currentTime = 0
+                            this.apresArretSon()
                         }
                     });
                     document.addEventListener('jouant', () => { 
@@ -36,10 +50,18 @@
 
             jouerSon() {
                 const sonAttribut = this.getAttribute("cheminduson");
-                const son = new Audio(sonAttribut);
+                this.son = new Audio(sonAttribut);
                 document.dispatchEvent(new Event('jouant'));
-                son.play()
-                son.onended = () => document.dispatchEvent(new Event('stoppey'));
+                this.son.play()
+                this.son.onended = () => {
+                    this.apresArretSon()
+                }
+            }
+
+            apresArretSon() {
+                this.playIcon.style.display = 'block'
+                this.stopIcon.style.display = 'none'
+                document.dispatchEvent(new Event('stoppey'));
             }
         },
     );
